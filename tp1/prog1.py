c5ce68ff642ab579adf15eb23de2ae7148446a83
Mervedi Lody <lodymervedipro@gmail.com>
2026-09-09 15:54:42 +0200

    ajout fonctions et tests

--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,2 +1,7 @@
+import fonctions as f
 print("Hello, World!")
-
+while True:
+    a = float(input("Entrez le premier nombre : "))
+    b = float(input("Entrez le deuxième nombre : "))
+    res = f.puissance(a, b)
+    print(res)
