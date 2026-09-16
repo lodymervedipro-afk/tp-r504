c9d7da8b3389a6442225f38c6ab019486c92052d
Mervedi Lody <lodymervedipro@gmail.com>
2026-09-16 14:03:47 +0200

    ajout gestion 0 puissance negative et tests cas limites

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,7 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
-        raise TypeError("que des entiers") 
-    else :
-    	return a ** b
+        raise TypeError("que des entiers")
+    if a == 0 and b < 0:
+        raise Exception("0 puissance négative est indéfini")
+    else:
+        return a ** b
--- TP1/test.py
+++ TP1/test.py
@@ -10,3 +10,6 @@ def test_2():
     assert f.puissance(-1,-1) == -1
     assert f.puissance(-1,-2) == 1
     assert f.puissance(-2,-1) == -0.5
+def test_3():
+    assert f.puissance(0,5) == 0
+    assert f.puissance(0,1) == 0
